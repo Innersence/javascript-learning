f38503e26e8aba7c62416caa0c4df1c4723ce938
Innersence <liyiping1988@126.com>
2016-11-30 15:16:20 +0800

    Update
    修正描述错误

--- src/js/prototype.js
+++ src/js/prototype.js
@@ -15,9 +15,9 @@
 	var Bar = function(){};
 	Bar.prototype = new Foo();
 	Bar.prototype.constructor =  new Bar();
-	Bar.prototype.value = { name: "proto value" };//不能将基本类型如1,"abc"等赋值给 prototype.value 
-
-	var bar = new Bar();
+	Bar.prototype.value = { name: "proto value" };//不能将基本类型如1,"abc"等直接赋值给 prototype，如
+                                                       //Bar.prototype =1 是不被允许的
+        var bar = new Bar();
 
 	console.log(bar.value);//proto value  --必须是new 出来的Bar才有value属性，Prototype中的新属性只能在new出的对象中才有
  // console.logBar.value);//这样会报错
