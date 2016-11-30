a44259fd2e5dcdd807f1e53bcd85b1ce1093bc6e
Innersence <liyiping1988@126.com>
2016-11-30 14:45:08 +0800

    Create prototype.js
    prtototype的赋值注意事项, 不能赋值给prototype基本属性
    prototype的新属性必须在new出来的对象中才有用

---
+++ src/js/prototype.js
@@ -0,0 +1,28 @@
+  /*
+  *prtototype的赋值注意事项
+  */
+  
+  function Foo() {
+        this.value = 42;
+    }
+
+    Foo.prototype = {
+        method: function() {
+        	console.log("This is method from Foo.");
+        }
+    };
+
+	var Bar = function(){};
+	Bar.prototype = new Foo();
+	Bar.prototype.constructor =  new Bar();
+	Bar.prototype.value = { name: "proto value" };//不能将基本类型如1,"abc"等赋值给 prototype.value 
+
+	var bar = new Bar();
+
+	console.log(bar.value);//proto value  --必须是new 出来的Bar才有value属性，Prototype中的新属性只能在new出的对象中才有
+ // console.logBar.value);//这样会报错
+  
+	Bar.value = {name:"raw value"};
+	console.log(Bar.value);// raw value
+  
+  //source : http://bonsaiden.github.io/JavaScript-Garden/zh/
